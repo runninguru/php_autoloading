b57870363411fc4605b9310b56473340eb83645a
Kyle Birch <theflyinginvalid@gmail.com>
2018-08-06 11:07:19 -0600

    Create autoload_classes.php

---
+++ autoload_classes.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |-------------------------------------------------------------------------------
+    | This is an exhaustive list of directories each of which contains 1 or more
+    | classes as direct children (not OOP children, but hierarchical children)
+    |--------------------------------------------------------------------------------
+    |
+    |
+    */
+
+    '/controllers/'
+    ,'/controllers/adminControllers/'
+    ,'/database/'
+    ,'/database/models/'
+    , '/library/'
+
+];
