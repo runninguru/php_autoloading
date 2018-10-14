81b29ddb755ea3742e8951da5cb5101482d6ece2
Kyle Birch <theflyinginvalid@gmail.com>
2018-10-14 16:56:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function($class) {
-	$directory_list = include $_SERVER['DOCUMENT_ROOT'] . '/autoload_paths.php';
+	$directory_list = include $_SERVER['DOCUMENT_ROOT'] . '/autoload_classes.php';
 	foreach($directory_list as $path){
 		if(file_exists($_SERVER['DOCUMENT_ROOT'] . $path . $class . '.php')){
 			include $_SERVER['DOCUMENT_ROOT'] . $path . $class . '.php';
